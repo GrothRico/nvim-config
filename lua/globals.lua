42d9403e02a115f6c874658c6c128006be3b8761
Rico Groth <groth.rico@protonmail.com>
2024-05-31 22:30:44 +0200

    fix path to find OS name, add harpoon plugin, add LSPs to ensure_installed

--- lua/globals.lua
+++ lua/globals.lua
@@ -1,22 +1,22 @@
 local function ReadFileAndClose(filepath)
-  local content = ''
-  local file = io.open(filepath, 'r')
-  if file then
-    content = file:read '*all'
-    file:close()
-  end
-  return content
+    local content = ""
+    local file = io.open(filepath, "r")
+    if file then
+        content = file:read("*all")
+        file:close()
+    end
+    return content
 end
 
-vim.g.isWindowsSystem = vim.fn.has 'win32' == 1
+vim.g.isWindowsSystem = vim.fn.has("win32") == 1
 vim.g.isLinuxSystem = (function()
-  return vim.loop.os_uname().sysname == 'Linux'
+    return vim.loop.os_uname().sysname == "Linux"
 end)()
 vim.g.isNixOSSystem = (function()
-  if not vim.g.isLinuxSystem then
-    return false
-  end
-  return string.find(ReadFileAndClose 'etc/os-release', 'ID=nixos')
+    if not vim.g.isLinuxSystem then
+        return false
+    end
+    return string.find(ReadFileAndClose("/etc/os-release"), "ID=nixos")
 end)()
 
 vim.g.have_nerd_font = true
